0877e69193cba7e34b039cd6b21076e8e7a6e63c
Anant Jaiswal <razorquake2499@gmail.com>
2024-02-22 17:29:26 +0530

    Added Search History feature and a few more sources

--- app/src/main/java/com/example/newz/presentation/common/SearchBar.kt
+++ app/src/main/java/com/example/newz/presentation/common/SearchBar.kt
@@ -23,7 +23,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.platform.SoftwareKeyboardController
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
